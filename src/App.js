92214f07beeb5e4456b0aed9346e8d1171db856c
Juliana Meneguete <jumeneguete@gmail.com>
2021-09-28 15:07:50 -0300

    feat: create route to sign up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+
 export default function App() {
   return (
-    <p>Hello World!</p>
+    <>
+    <Router>
+      <Switch>
+        <Route path="/sign-up" exact>
+          <SignUp />
+        </Route>
+      </Switch>
+    </Router>
+    </>
   );
 }
